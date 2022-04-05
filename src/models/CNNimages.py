f750b15647fa2e52298e6d87db21b447f8df80a8
Vanessa Hadlock <vhadlock@starry.com>
2022-04-05 16:22:59 -0400

    initial commit with directories and first model file

---
+++ src/models/CNNimages.py
@@ -0,0 +1,9 @@
+'''CNN for plastic detection image data
+Training a CNN to detect plastic or no plastic using (Resnet?) '''
+
+import os 
+import tensorflow as tf 
+from tensorflow.keras.preprocessing.image import ImageDataGenerator 
+from tensorflow.keras import layers 
+from tensorflow.keras import Model 
+import matplotlib.pyplot as plt
